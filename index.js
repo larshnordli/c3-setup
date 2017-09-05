a88a6d1f77beffabb49900b62b752fda3aca021d
Lars Henrik Nordli <lars.henrik.nordli@accenture.com>
2017-09-05 14:22:47 +0200

    added runtime

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('successfully setup new backend app');
+var http = require('http');
+
+var server = http.createServer(function (req, res) {
+    res.writeHead(200);
+    console.log('Successfully created backend');
+    res.end('Hello Http');
+});
+server.listen(8080);
